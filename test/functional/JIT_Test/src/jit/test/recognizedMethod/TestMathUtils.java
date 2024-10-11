12d3c86a91ca32fbbf376bf4ef1091d063df3f12
Matthew Hall <matthew.hall3@outlook.com>
2024-10-11 11:02:56 -0400

    t

--- test/functional/JIT_Test/src/jit/test/recognizedMethod/TestMathUtils.java
+++ test/functional/JIT_Test/src/jit/test/recognizedMethod/TestMathUtils.java
@@ -131,7 +131,7 @@ public class TestMathUtils {
             {dsNaN, -0.0d, dsNaNBits}
         };
 
-        Object[][] nanPairs = new Object[60][3];
+        Object[][] nanPairs = new Object[100][3];
         int i = 0;
         while (i < constNanPairs.length) {
             nanPairs[i] = constNanPairs[i];
