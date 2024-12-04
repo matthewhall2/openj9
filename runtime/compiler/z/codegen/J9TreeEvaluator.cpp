487748b1b601453497e1b4c0e8e9ea321f40a582
Matthew Hall <matthew.hall3@outlook.com>
2024-12-04 13:21:49 -0500

    test depth

--- runtime/compiler/z/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/z/codegen/J9TreeEvaluator.cpp
@@ -11813,6 +11813,7 @@ TR::Register *J9::Z::TreeEvaluator::inlineCheckAssignableFromEvaluator(TR::Node 
    if (genTrap)
       {
       generateRIEInstruction(cg, TR::InstOpCode::CIT, node, resultReg, 1, TR::InstOpCode::COND_BE);
+      generateRIEInstruction(cg, TR::InstOpCode::CIT, node, resultReg, 1, TR::InstOpCode::COND_BE);
       }
 
 
@@ -11834,7 +11835,7 @@ TR::Register *J9::Z::TreeEvaluator::inlineCheckAssignableFromEvaluator(TR::Node 
    // no need to check for null inline, NULLCHECK nodes are inserted during the inlined called recognition
    genInlineClassEqualityTest(node, cg, cg->comp(), toClassReg, fromClassReg, doneLabel);
    TR::Register *scratchReg1, *scratchReg2;
-   scratchReg1 = cg->allocateRegister();
+   scratchReg1 = cg->allocateRegister();:
    scratchReg2 = cg->allocateRegister();
    
    deps->addPostCondition(scratchReg1, TR::RealRegister::AssignAny);
