75e5167cca5c9b5c75770e24e742b27340d7eb6a
Matthew Hall <matthew.hall3@outlook.com>
2025-03-03 14:34:37 -0500

    add test runner
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

---
+++ test/functional/Jsr335/src/org/openj9/test/jsr335/defendersupersends/asm/TestRunner.java
@@ -0,0 +1,20 @@
+package org.openj9.test.jsr335.defendersupersends.asm;
+
+public class TestRunner {
+        public static void main(String[] args)
+        {
+        System.out.println("Start");
+        System.out.println("Test");
+        try {
+        TestDefenderMethodLookupAsm test = new TestDefenderMethodLookupAsm();
+        System.out.println(test);
+        test.testConflictingDefinitionsInSuperInterface();
+        test.testConflictingDefinitionsInSuperInterface();
+
+//      test.test335DefenderSupersendsAsmAsMethodHandles();
+//      test.test335DefenderSupersendsAsmAsMethodHandles();
+        } catch (Throwable t) {
+                System.out.println("error");
+        }
+}
+}
