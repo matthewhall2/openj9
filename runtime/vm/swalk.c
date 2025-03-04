605cb898f650cd948645b23aeaee24e70e191300
Matthew Hall <matthew.hall3@outlook.com>
2025-03-04 13:18:10 -0500

    add debug temps
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/vm/swalk.c
+++ runtime/vm/swalk.c
@@ -987,7 +987,7 @@ walkBytecodeFrame(J9StackWalkState * walkState)
 		J9VMThread *walkThread = walkState->walkThread;
 		J9Method *currentDefault = current->javaVM->initialMethods.throwDefaultConflict;
 		J9Method *walkDefault = walkThread->javaVM->initialMethods.throwDefaultConflict;
-		if (false){
+		if (0){
 			printf("%p %p %p %p %p", m, current, walkThread, currentDefault, walkDefault);
 		}
 		if (walkState->method == walkState->walkThread->javaVM->initialMethods.throwDefaultConflict)
