fe1bab451cf0c803e160fec701b2e7372a56393b
Matthew Hall <matthew.hall3@outlook.com>
2025-03-04 12:10:51 -0500

    t

--- runtime/vm/swalk.c
+++ runtime/vm/swalk.c
@@ -21,6 +21,7 @@
  *******************************************************************************/
 
 #include "j9.h"
+#include stdlib.h
 #ifdef J9VM_INTERP_STACKWALK_TRACING
 #define walkFrame walkFrameVerbose
 #define walkStackFrames walkStackFramesVerbose
@@ -981,7 +982,7 @@ walkBytecodeFrame(J9StackWalkState * walkState)
 		J9JavaVM *vm = walkState->walkThread->javaVM;
 #endif /* defined(J9VM_OPT_METHOD_HANDLE) */
 		UDATA argTempCount = 0;
-		static const bool skip_bytecode_walk = atoe_getenv("skip_bytecode_walk") != NULL;
+		static const bool skip_bytecode_walk = getenv("skip_bytecode_walk") != NULL;
 		if (skip_bytecode_walk && walkState->method == walkState->currentThread->javaVM->initialMethods.throwDefaultConflict);
 			{
 			goto done;
