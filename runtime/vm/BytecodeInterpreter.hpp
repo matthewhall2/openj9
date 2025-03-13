462b9436094b868f1b6b476dee37d353b529dedb
Matthew Hall <matthew.hall3@outlook.com>
2025-03-13 16:37:19 -0400

    t

--- runtime/vm/BytecodeInterpreter.hpp
+++ runtime/vm/BytecodeInterpreter.hpp
@@ -628,7 +628,6 @@
 		 bool check_in_j2i = getenv("check_in_j2i") != NULL;
 		 if (check_in_j2i) {
 		 if (isMethodDefaultConflictJ9Method(_sendMethod)) {
-			 buildJITResolveFrame(REGISTER_ARGS);
 			 return GOTO_RUN_METHOD;
 		 }
 	 }
