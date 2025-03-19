6bcc938417b4b62099eaed0b1e32c2ebe313dbb1
Matthew Hall <matthew.hall3@outlook.com>
2025-03-19 15:58:34 -0400

    dont set sendmethod
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/vm/BytecodeInterpreter.hpp
+++ runtime/vm/BytecodeInterpreter.hpp
@@ -9690,7 +9690,7 @@ throwDefaultConflict:
 					_currentThread->literals = _currentThread->javaVM->initialMethods.throwDefaultConflict;
 					}
 			}
-			_sendMethod = method
+			_sendMethod = method;
 			// run() will run throwDefaultConflictForMemberName()
 			return GOTO_RUN_METHOD;
 		}
