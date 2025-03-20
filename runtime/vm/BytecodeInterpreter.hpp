bb1dcc44ccd193dad89853bf2803563b6a401178
Matthew Hall <matthew.hall3@outlook.com>
2025-03-20 15:01:48 -0400

    set arg count
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/vm/BytecodeInterpreter.hpp
+++ runtime/vm/BytecodeInterpreter.hpp
@@ -9684,7 +9684,8 @@ done:
 				if (nullCheckJ9Obj(mhReceiver, false, REGISTER_ARGS, false) == THROW_NPE) return THROW_NPE;
 			}
 		} else {
-			methodArgCount = (UDATA)atoi(getenv("argcount"));
+			char *c = getenv("argcount");
+			methodArgCount = c ? (UDATA)atoi(c) : 0;
 		//	goto throwDefaultConflict;
 		}
 
@@ -9727,19 +9728,19 @@ done:
 
 		return rc;
 
-throwDefaultConflict:
-		if (fromJIT) {
-			_sp -= 1;
-			if (getenv("build_frame_lts") != NULL) {
-			buildJITResolveFrame(REGISTER_ARGS);
-			}
-			if (getenv("set_literals_lts") != NULL) {
-				_literals = _currentThread->javaVM->initialMethods.throwDefaultConflict;
-				_currentThread->literals = _currentThread->javaVM->initialMethods.throwDefaultConflict;
-				}
-		}
-		// run() will run throwDefaultConflictForMemberName()
-		return GOTO_RUN_METHOD;
+// throwDefaultConflict:
+// 		if (fromJIT) {
+// 			_sp -= 1;
+// 			if (getenv("build_frame_lts") != NULL) {
+// 			buildJITResolveFrame(REGISTER_ARGS);
+// 			}
+// 			if (getenv("set_literals_lts") != NULL) {
+// 				_literals = _currentThread->javaVM->initialMethods.throwDefaultConflict;
+// 				_currentThread->literals = _currentThread->javaVM->initialMethods.throwDefaultConflict;
+// 				}
+// 		}
+// 		// run() will run throwDefaultConflictForMemberName()
+// 		return GOTO_RUN_METHOD;
 	}
 
 	VMINLINE VM_BytecodeAction
