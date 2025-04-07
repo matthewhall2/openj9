40d44e36d30f3ec438f74eb2be5a25953335094e
Matthew Hall <matthew.hall3@outlook.com>
2025-04-07 16:31:35 -0400

    add new default
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/oti/j9nonbuilder.h
+++ runtime/oti/j9nonbuilder.h
@@ -3365,7 +3365,7 @@ typedef struct J9InitializerMethods {
 	struct J9Method* invokePrivateMethod;
 #if defined(J9VM_OPT_OPENJDK_METHODHANDLE)
 	struct J9Method* throwDefaultConflict;
-	struct J9ROMethod* defaultConflictROMMethod;
+	struct J9ROMMethod* defaultConflictROMMethod;
 #endif /* defined(J9VM_OPT_OPENJDK_METHODHANDLE) */
 } J9InitializerMethods;
 
