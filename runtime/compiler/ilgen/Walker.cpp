ac29fe6f5a73b731f80edd9d9a8c416ddfc00e6b
Matthew Hall <matthew.hall3@outlook.com>
2025-05-01 15:35:52 -0400

    get like other invvokes

--- runtime/compiler/ilgen/Walker.cpp
+++ runtime/compiler/ilgen/Walker.cpp
@@ -3252,7 +3252,7 @@ TR_J9ByteCodeIlGenerator::genInvokeDynamic(int32_t callSiteIndex)
    if (isUnresolved)
       targetMethodSymRef->getSymbol()->setDummyResolvedMethod(); // linkToStatic is a dummy TR_ResolvedMethod
 
-   TR::SymbolReference *methodSymRef = symRefTab()->findOrCreateStaticMethodSymbol(_methodSymbol, cpIndex);
+   TR::SymbolReference *methodSymRef = symRefTab()->findOrCreateStaticMethodSymbol(_methodSymbol, next2Bytes());
 //   auto owningMethod = (TR_ResolvedJ9Method*)_methodSymbol->getResolvedMethod();
 
   // TR::SymbolReference *methodSymRef = symRefTab()->findOrCreateStaticMethodSymbol(_methodSymbol, next2Bytes());
