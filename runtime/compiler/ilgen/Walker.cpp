2b551942c9d7296899fce22ce7b6d4a7a5a6262c
Matthew Hall <matthew.hall3@outlook.com>
2025-05-01 13:18:08 -0400

    trace
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/ilgen/Walker.cpp
+++ runtime/compiler/ilgen/Walker.cpp
@@ -3253,7 +3253,7 @@ TR_J9ByteCodeIlGenerator::genInvokeDynamic(int32_t callSiteIndex)
       targetMethodSymRef->getSymbol()->setDummyResolvedMethod(); // linkToStatic is a dummy TR_ResolvedMethod
    
    int32_t len = 0;
-   // const char* sig = targetMethodSymRef->getTypeSignature(len);
+   const char* sig = targetMethodSymRef->getTypeSignature(len);
    printf("---> genInvokeDyn: TypeSig - %.*s\n", len, sig);
 
    TR::SymbolReference *callSiteTableEntrySymRef = symRefTab()->findOrCreateCallSiteTableEntrySymbol(_methodSymbol, callSiteIndex);
