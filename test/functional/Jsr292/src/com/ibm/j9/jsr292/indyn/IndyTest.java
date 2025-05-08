8523bb47eb021caa1c3fc9351ddea39b63cc873b
Matthew Hall <matthew.hall3@outlook.com>
2025-05-08 14:45:17 -0400

    add const call site

--- test/functional/Jsr292/src/com/ibm/j9/jsr292/indyn/IndyTest.java
+++ test/functional/Jsr292/src/com/ibm/j9/jsr292/indyn/IndyTest.java
@@ -32,8 +32,8 @@ import static org.objectweb.asm.Opcodes.*;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.invoke.ConstantCallSite.*;
-import java.lang.invoke.CallSite.*;
+import java.lang.invoke.ConstantCallSite;
+import java.lang.invoke.CallSite;
 import java.lang.invoke.MethodType;
 import java.util.ArrayList;
 import java.util.Arrays;
