3927e772ca58203e517dd09ee00ed66a04c56583
Matthew Hall <matthew.hall3@outlook.com>
2025-05-09 13:31:26 -0400

    try to force osr

--- test/functional/Jsr292/src/com/ibm/j9/jsr292/indyn/IndyTest.java
+++ test/functional/Jsr292/src/com/ibm/j9/jsr292/indyn/IndyTest.java
@@ -469,9 +469,6 @@ public class IndyTest {
         mv.visitVarInsn(ISTORE, 0);       // acc = acc - i
 		mv.visitLabel(afterCondition);
 
-		mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-		mv.visitVarInsn(ILOAD, 0); // load acc (slot 0)
-		mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(I)V", false);
 
 		// i++
         mv.visitIincInsn(1, 1);           // i = i + 1
