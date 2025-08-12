ac614d4d69c4b1a6dac7c90961edbc3f5f2334b5
Matthew Hall <matthew.hall3@outlook.com>
2025-08-12 13:50:29 -0400

    check for def con in virtual always
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/jcl/common/java_lang_invoke_MethodHandleNatives.cpp
+++ runtime/jcl/common/java_lang_invoke_MethodHandleNatives.cpp
@@ -1212,13 +1212,13 @@ Java_java_lang_invoke_MethodHandleNatives_resolve(
 							/* Load special sendTarget to throw the exception during invocation */
 							target = JLONG_FROM_POINTER(vm->initialMethods.throwDefaultConflict);
 							if (getenv("setDefConInResolve") != NULL) {
-								J9Method *throwDefaultConflictMethod = (J9Method*)target;
+								J9Method *throwDefaultConflictMethod = (J9Method*)JLONG_TO_POINTER(target);
 								throwDefaultConflictMethod->constantPool = J9_CP_FROM_CLASS(resolvedClass);
 								if (getenv("setBytecdes") != NULL) {
 									throwDefaultConflictMethod->bytecodes = method->bytecodes;
 								}
 								if (getenv("setRunAddress")) {
-									throwDefaultConflictMethod->methodRunAddress = J9_BCLOOP_ENCODE_SEND_TARGET(J9_BCLOOP_SEND_TARGET_MEMBERNAME_DEFAULT_CONFLICT);
+									throwDefaultConflictMethod->methodRunAddress = (getenv("useJ2I") != NULL) ? J9_BCLOOP_ENCODE_SEND_TARGET(J9_BCLOOP_SEND_TARGET_I2J_TRANSITION):  J9_BCLOOP_ENCODE_SEND_TARGET(J9_BCLOOP_SEND_TARGET_MEMBERNAME_DEFAULT_CONFLICT);
 								}
 							}
 							if (getenv("setMethodOnDefCon") != NULL) {
