707070e30923340421928939200e0d712140c8ff
Matthew Hall <matthew.hall3@outlook.com>
2025-08-19 16:35:19 -0400

    define func
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -2615,7 +2615,7 @@ J9::Z::PrivateLinkage::buildDirectCall(TR::Node * callNode, TR::SymbolReference 
             generateS390MemoryReference(scratchReg, -4, cg()));
       generateRSInstruction(cg(), TR::InstOpCode::getShiftRightLogicalSingleOpCode(), callNode, j9MethodReg, 16);
       generateRRInstruction(cg(), TR::InstOpCode::getAddRegOpCode(), callNode, scratchReg, j9MethodReg);
-      generateRRInstruction(cg(), TR::InstOpCode::BASR, callNode, getRealRegister(getReturnAddressRegister(), j9MethodReg, scratchReg);
+      generateRRInstruction(cg(), TR::InstOpCode::BASR, callNode, getRealRegister(getReturnAddressRegister()), j9MethodReg, scratchReg);
       
       TR::LabelSymbol *snippetLabel = generateLabelSymbol(cg());
       TR::Snippet *snippet = new (trHeapMemory()) TR::S390J9CallSnippet(cg(), callNode, snippetLabel, callSymRef, argSize);
