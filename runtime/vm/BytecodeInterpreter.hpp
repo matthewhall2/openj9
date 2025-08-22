4f6e009b26c1d84aebf1d9888efcac0a85d0351f
Matthew Hall <matthew.hall3@outlook.com>
2025-08-22 17:39:02 -0400

    var arg count
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/vm/BytecodeInterpreter.hpp
+++ runtime/vm/BytecodeInterpreter.hpp
@@ -450,7 +450,7 @@ retry:
 		UDATA *bp = buildSpecialStackFrame(REGISTER_ARGS, J9SF_FRAME_TYPE_METHOD, flags, false);
 		*--_sp = (UDATA)method;
 		UDATA argCount = getenv("argCount") != NULL ? atoi(getenv("argCount")) : 0xffffffff;
-		_arg0EA = bp + (UDATA)(argCount != 0xffffffff ? argCount : _currentThread->floatTemp1);
+		_arg0EA = bp + (UDATA)(argCount != 0xffffffff ? argCount : (UDATA)_currentThread->floatTemp1);
 		return bp;
 	}
 
