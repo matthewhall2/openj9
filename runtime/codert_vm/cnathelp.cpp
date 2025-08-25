3f2706955c537ce1b025ebe0a35245efa1da162e
Matthew Hall <matthew.hall3@outlook.com>
2025-08-25 11:26:56 -0400

    t
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/codert_vm/cnathelp.cpp
+++ runtime/codert_vm/cnathelp.cpp
@@ -3401,15 +3401,12 @@ old_slow_icallVMprJavaSendPatchupVirtual(J9VMThread *currentThread)
 	UDATA thunk = 0;
 #if defined(J9VM_OPT_OPENJDK_METHODHANDLE)
 	if (J9_BCLOOP_SEND_TARGET_METHODHANDLE_INVOKEBASIC == J9_BCLOOP_DECODE_SEND_TARGET(method->methodRunAddress)) {
-		printf("adding invokeBasic callsite\n");
 		/* Because invokeBasic() is signature-polymorphic, the signature from the ROM method is irrelevant.
 		 * We need the J2I thunk that corresponds to the signature that the JIT was using at the call site.
 		 * Since this varies depending on the call site, we can't replace the VFT entry with the J2I thunk.
 		 */
 		J9JITInvokeBasicCallSite *site = jitGetInvokeBasicCallSiteFromPC(currentThread, (UDATA)jitReturnAddress);
 		thunk = (UDATA)site->j2iThunk;
-	} else if (J9_BCLOOP_SEND_TARGET_MEMBERNAME_DEFAULT_CONFLICT == J9_BCLOOP_DECODE_SEND_TARGET(method->methodRunAddress)) {
-		printf("mh def con send target\n");
 	}
 	else
 #endif /* defined(J9VM_OPT_OPENJDK_METHODHANDLE) */
