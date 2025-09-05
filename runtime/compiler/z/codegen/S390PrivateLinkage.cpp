51e2897584f7e5b3457b04c9a1b3034d0d4ad187
Matthew Hall <matthew.hall3@outlook.com>
2025-09-05 12:11:01 -0400

    unresolved call
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -2637,7 +2637,7 @@ J9::Z::PrivateLinkage::buildDirectCall(TR::Node * callNode, TR::SymbolReference 
       
      TR::SymbolReference *snippetSyRref = new (trHeapMemory()) TR::SymbolReference(
         comp()->getSymRefTab(), snippetLabel);
-         TR::Snippet *snippet = new (trHeapMemory()) TR::S390UnresolvedCallSnippet(cg(), callNode, snippetLabel, argSize);
+         TR::Snippet *snippet = new (trHeapMemory()) TR::S390J9CallSnippet(cg(), callNode, snippetLabel, callSymRef, argSize);
       //  TR::Snippet * snippet = new (trHeapMemory()) TR::S390HelperCallSnippet(cg(), callNode, snippetLabel,
       //                                                     callNode->getSymbolReference(), doneSnippetLabel, argSize);
       cg()->addSnippet(snippet);
