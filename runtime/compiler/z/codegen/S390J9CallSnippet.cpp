7cdaab2f3909cd894838f5a224f3341cad58162c
Matthew Hall <matthew.hall3@outlook.com>
2025-09-08 15:02:55 -0400

    no reloc
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390J9CallSnippet.cpp
+++ runtime/compiler/z/codegen/S390J9CallSnippet.cpp
@@ -421,16 +421,16 @@ TR::S390J9CallSnippet::emitSnippetBody()
             {
             // int32_t disp32 = cg()->branchDisplacementToHelperOrTrampoline(cursor, glueRef);
             // *(int32_t *)(++cursor) = disp32;
-            cg()->addExternalRelocation(
-         TR::ExternalRelocation::create(
-            cursor,
-            (uint8_t *)glueRef,
-            TR_HelperAddress,
-            cg()),
-         __FILE__,
-         __LINE__,
-         getNode());
-      cursor += 4;
+      //       cg()->addExternalRelocation(
+      //    TR::ExternalRelocation::create(
+      //       cursor,
+      //       (uint8_t *)glueRef,
+      //       TR_HelperAddress,
+      //       cg()),
+      //    __FILE__,
+      //    __LINE__,
+      //    getNode());
+      // cursor += 4;
            //  cg()->addProjectSpecializedRelocation(cursor, (uint8_t *)glueRef, NULL, TR_HelperAddress, __FILE__, __LINE__,
       //  callNode);
             }
