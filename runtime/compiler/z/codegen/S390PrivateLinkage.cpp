68aecb4566eea90029a86b2c2187183d8ff1f8f8
Matthew Hall <matthew.hall3@outlook.com>
2025-09-10 10:37:44 -0400

    test

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -3568,7 +3568,7 @@ J9::Z::PrivateLinkage::addSpecialRegDepsForBuildArgs(TR::Node * callNode, TR::Re
       TR::Register *specialArg = copyArgRegister(callNode, child, cg()->evaluate(child)); // TODO:JSR292: We don't need a copy of the highOrder reg on 31-bit
       if (specialArg->getRegisterPair())
          specialArg = specialArg->getLowOrder(); // on 31-bit, the top half doesn't matter, so discard it
-      dependencies->addPreCondition(specialArg, specialArgReg, cg() );
+      dependencies->addPreCondition(specialArg, specialArgReg);
       cg()->decReferenceCount(child);
 
       if (comp()->getOption(TR_TraceCG))
