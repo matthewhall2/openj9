276d9d04b1bc89876cf579f26a85264a848b38d0
Matthew Hall <matthew.hall3@outlook.com>
2025-10-16 11:40:21 -0400

    no extra work for 32
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/x/codegen/X86PrivateLinkage.cpp
+++ runtime/compiler/x/codegen/X86PrivateLinkage.cpp
@@ -2044,7 +2044,6 @@ void J9::X86::PrivateLinkage::buildDirectCall(
       TR::Register *scratchReg = cg()->allocateRegister();
       site.addPostCondition(
          scratchReg, getProperties().getVTableIndexArgumentRegister());
-
       // This will be assigned to getJ9MethodArgumentRegister().
 
       TR::Register *j9mReg = callNode->getChild(0)->getRegister();
@@ -2074,6 +2073,7 @@ void J9::X86::PrivateLinkage::buildDirectCall(
 
       generateLabelInstruction(oolBranchOp, callNode, interpreterCallLabel, cg());
 
+      if (comp()->target().is64Bit()) {
       // The method is compiled - call through register to JIT entry point
       generateRegMemInstruction(
          TR::InstOpCode::L4RegMem,
@@ -2087,6 +2087,7 @@ void J9::X86::PrivateLinkage::buildDirectCall(
 
       generateRegRegInstruction(
          TR::InstOpCode::ADDRegReg(), callNode, scratchReg, j9mReg, cg());
+      }
 
       callInstr = generateRegInstruction(
          TR::InstOpCode::CALLReg, callNode, scratchReg, cg());
