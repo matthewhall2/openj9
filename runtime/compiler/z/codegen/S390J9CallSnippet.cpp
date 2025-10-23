fa3c68c56ce47b1085325b7b588d8be87a0f047b
Matthew Hall <matthew.hall3@outlook.com>
2025-10-23 18:20:59 -0400

    fix
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390J9CallSnippet.cpp
+++ runtime/compiler/z/codegen/S390J9CallSnippet.cpp
@@ -271,23 +271,6 @@ TR::S390J9CallSnippet::S390flushArgumentsToStack(uint8_t *buffer, TR::Node *call
    }
 
 uint8_t *
-TR::S390J9HelperCallSnippet::emitSnippetBody() {
-   uint8_t *cursor = cg()->getBinaryBufferCursor();
-   getSnippetLabel()->setCodeLocation(cursor);
-
-   TR::Node *callNode = getNode();
-   TR::SymbolReference *helperSymRef = getHelperSymRef();
-   bool jitInduceOSR = helperSymRef->isOSRInductionHelper();
-   bool isJitDispatchJ9Method = callNode->isJitDispatchJ9MethodCall(cg()->comp());
-   if (jitInduceOSR || isJitDispatchJ9Method) {
-      // Flush in-register arguments back to the stack for interpreter
-      cursor = TR::S390J9CallSnippet::S390flushArgumentsToStack(cursor, callNode, getSizeOfArguments(), cg());
-   }
-
-   TR::S390HelperCallSnippet::emitSnippetBodyHelper(cursor, helperSymRef);
-}
-
-uint8_t *
 TR::S390J9CallSnippet::emitSnippetBody()
    {
    TR::Compilation *comp = cg()->comp();
