80320af9898736bc76e05ec43c81f412306b41b1
Matthew Hall <matthew.hall3@outlook.com>
2025-10-23 18:35:52 -0400

    test
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390J9HelperCallSnippet.cpp
+++ runtime/compiler/z/codegen/S390J9HelperCallSnippet.cpp
@@ -1,4 +1,21 @@
+#include "z/codegen/S390J9HelperCallSnippet.hpp"
+#include "z/codegen/CallSnippet.hpp"
+#include "z/codegen/S390J9CallSnippet.hpp"
+
+
 uint8_t *
 TR::S390J9HelperCallSnippet::emitSnippetBody() {
-    
+   uint8_t *cursor = cg()->getBinaryBufferCursor();
+   getSnippetLabel()->setCodeLocation(cursor);
+
+   TR::Node *callNode = getNode();
+   TR::SymbolReference *helperSymRef = getHelperSymRef();
+   bool jitInduceOSR = helperSymRef->isOSRInductionHelper();
+   bool isJitDispatchJ9Method = callNode->isJitDispatchJ9MethodCall(cg()->comp());
+   if (jitInduceOSR || isJitDispatchJ9Method) {
+      // Flush in-register arguments back to the stack for interpreter
+      cursor = TR::S390J9CallSnippet::S390flushArgumentsToStack(cursor, callNode, getSizeOfArguments(), cg());
+   }
+
+   return TR::S390HelperCallSnippet::emitSnippetBodyHelper(cursor, helperSymRef);
 }
