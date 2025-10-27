9897ff52241f75085dd0a1d8321211008fe876a4
Matthew Hall <matthew.hall3@outlook.com>
2025-10-27 14:02:52 -0400

    interp deps
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -2606,21 +2606,22 @@ J9::Z::PrivateLinkage::buildDirectCall(TR::Node * callNode, TR::SymbolReference 
       preDeps->setNumPostConditions(0, trMemory());
       preDeps->setAddCursorForPost(0);
 
-      TR::RegisterDependencyConditions * postDeps = new (trHeapMemory()) TR::RegisterDependencyConditions(1, 5, cg());
-      postDeps->addPostCondition(j9MethodReg, TR::RealRegister::AssignAny);
-      postDeps->addPostCondition(scratchReg, getVTableIndexArgumentRegister());
-      postDeps->addPostCondition(scratchReg2, TR::RealRegister::AssignAny);
-
-
-      // TR::RegisterDependencyConditions *postDeps = new (trHeapMemory()) TR::RegisterDependencyConditions(1, 5, cg());
-      // postDeps->setNumPostConditions(5, trMemory());
-      // postDeps->setAddCursorForPost(0);
+      // TR::RegisterDependencyConditions * postDeps = new (trHeapMemory()) TR::RegisterDependencyConditions(1, 5, cg());
       // postDeps->addPostCondition(j9MethodReg, TR::RealRegister::AssignAny);
       // postDeps->addPostCondition(scratchReg, getVTableIndexArgumentRegister());
       // postDeps->addPostCondition(scratchReg2, TR::RealRegister::AssignAny);
 
+
+      TR::RegisterDependencyConditions *postDeps = new (trHeapMemory()) TR::RegisterDependencyConditions(1, 5, cg());
+      postDeps->setNumPostConditions(5, trMemory());
+      postDeps->setAddCursorForPost(0);
+      postDeps->addPostConditionIfNotAlreadyInserted()
+      postDeps->addPostCondition(j9MethodReg, TR::RealRegister::AssignAny);
+      postDeps->addPostCondition(scratchReg, getVTableIndexArgumentRegister());
+      postDeps->addPostCondition(scratchReg2, TR::RealRegister::AssignAny);
+
       
-      TR::RegisterDependencyConditions *interpreterdDeps = new (trHeapMemory()) TR::RegisterDependencyConditions(dependencies, 0, 2, cg());
+      TR::RegisterDependencyConditions *interpreterdDeps = new (trHeapMemory()) TR::RegisterDependencyConditions(preDeps, 0, 2, cg());
       interpreterdDeps->addPostConditionIfNotAlreadyInserted(scratchReg, getVTableIndexArgumentRegister());
       interpreterdDeps->addPostConditionIfNotAlreadyInserted(j9MethodReg, TR::RealRegister::AssignAny);
 
