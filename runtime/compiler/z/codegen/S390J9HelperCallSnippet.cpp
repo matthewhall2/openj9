6c7a450e79bb2c83770aec026fb6c59c01cdae69
Matthew Hall <matthew.hall3@outlook.com>
2025-10-29 09:45:46 -0400

    test conditional snippet
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390J9HelperCallSnippet.cpp
+++ runtime/compiler/z/codegen/S390J9HelperCallSnippet.cpp
@@ -16,10 +16,28 @@ TR::S390J9HelperCallSnippet::emitSnippetBody() {
       // Flush in-register arguments back to the stack for interpreter
       cursor = TR::S390J9CallSnippet::S390flushArgumentsToStack(cursor, callNode, getSizeOfArguments(), cg());
    }
-   
-   // LGR R1 R7
-   *(int32_t *)cursor = 0xB9040017;
-   cursor += sizeof(int32_t);
+
+   /* The J9Method pointer to be passed to be interpreter is in R7, but the interpreter expects it to be in R1.
+    * Since the first integer argument register is also R1, we only load it after we have flushed the args to the stack.
+    *
+    * 64 bit:
+    *    LGR R1, R7
+    * 32 bit:
+    *    LR R1, R7
+    */
+   if (getNode()->isJitDispatchJ9MethodCall(comp()))
+      {
+      if (comp()->target().is64Bit())
+         {
+         *(int32_t *)cursor = 0xB9040017;
+         cursor += sizeof(int32_t);
+         }
+      else
+         {
+         *(int16_t *)cursor = 0x1817;
+         cursor += sizeof(int16_t);
+         }
+      }
 
    return emitSnippetBodyHelper(cursor, helperSymRef);
 }
