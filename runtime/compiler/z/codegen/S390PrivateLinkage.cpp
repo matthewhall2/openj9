b818d331068dcf905bfae220c196b54913557a63
Matthew Hall <matthew.hall3@outlook.com>
2025-11-05 12:55:31 -0500

    test no copy
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -3490,6 +3490,7 @@ J9::Z::PrivateLinkage::addSpecialRegDepsForBuildArgs(TR::Node * callNode, TR::Re
       if (specialArg->getRegisterPair())
          specialArg = specialArg->getLowOrder(); // on 31-bit, the top half doesn't matter, so discard it
       dependencies->addPreCondition(specialArg, specialArgReg);
+      from += step;
    }
 
    if (specialArgReg != TR::RealRegister::NoReg)
