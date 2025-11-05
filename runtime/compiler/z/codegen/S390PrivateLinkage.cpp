a4dd0439d31de9eda8e4db63892bcd55e05528ef
Matthew Hall <matthew.hall3@outlook.com>
2025-11-05 13:07:23 -0500

    test ref count
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -3490,7 +3490,9 @@ J9::Z::PrivateLinkage::addSpecialRegDepsForBuildArgs(TR::Node * callNode, TR::Re
       if (specialArg->getRegisterPair())
          specialArg = specialArg->getLowOrder(); // on 31-bit, the top half doesn't matter, so discard it
       dependencies->addPreCondition(specialArg, specialArgReg);
+      callNode->getChild(from)->decReferenceCount();
       from += step;
+
    }
 
    if (specialArgReg != TR::RealRegister::NoReg)
