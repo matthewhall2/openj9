3f4a7c8bd070867e94d71b5d58deb90fa8411827
Matthew Hall <matthew.hall3@outlook.com>
2025-11-19 12:36:10 -0500

    change flag set
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
+++ runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
@@ -2913,7 +2913,7 @@ void J9::Power::PrivateLinkage::buildDirectCall(TR::Node *callNode,
       // gr11 and gr12 will never contain an object ref in this sequence, and may contain values such as
       // the J9Method::extra field value, which is invalid for gc
       flags &= ~TR::RealRegister::gr11Mask;
-     // flags &= ~TR::RealRegister::gr12Mask;
+      flags &= ~TR::RealRegister::gr12Mask;
 
       TR::Register *scratchReg = dependencies->searchPostConditionRegister(pp.getVTableIndexArgumentRegister());
       TR::Register *scratchReg2 = cg()->allocateRegister();
@@ -2959,7 +2959,7 @@ void J9::Power::PrivateLinkage::buildDirectCall(TR::Node *callNode,
       // test if compiled
       generateTrg1MemInstruction(cg(), TR::InstOpCode::Op_load, callNode, scratchReg,
                                  TR::MemoryReference::createWithDisplacement(cg(), j9MethodReg, offsetof(J9Method, extra), TR::Compiler->om.sizeofReferenceAddress()));
-      generateTrg1Src1ImmInstruction(cg(), TR::InstOpCode::andi_r, callNode, scratchReg2, scratchReg, 7);
+      generateTrg1Src1ImmInstruction(cg(), TR::InstOpCode::andi_r, callNode, scratchReg2, scratchReg, 1);
       // branch to ool if J9_STARTPC_NOT_TRANSLATED is set
       gcPoint = generateConditionalBranchInstruction(cg(), TR::InstOpCode::bne, callNode, oolLabel, cndReg);
       gcPoint->PPCNeedsGCMap(flags);
