2c372ba7f557446926e065f16e6ea933b3864422
Matthew Hall <matthew.hall3@outlook.com>
2025-11-20 16:18:12 -0500

    r0
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
+++ runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
@@ -2931,7 +2931,7 @@ void J9::Power::PrivateLinkage::buildDirectCall(TR::Node *callNode,
 
   //    dependencies->addPostCondition(scratchReg, TR::RealRegister::gr12);
       TR::RegisterDependencyConditions *newPostDeps = new (trHeapMemory()) TR::RegisterDependencyConditions(0, 1, trMemory());
-      newPostDeps->addPostCondition(scratchReg2, TR::RealRegister::NoReg);
+      newPostDeps->addPostCondition(scratchReg2, TR::RealRegister::gr0);
    //  newPostDeps->addPostCondition(j9MethodReg, TR::RealRegister::NoReg);
 
       TR::RegisterDependencyConditions *postDeps = dependencies->clone(cg(), newPostDeps);
