a8413e5726fd6008335ddf52f306b74e6cf168f3
Matthew Hall <matthew.hall3@outlook.com>
2025-12-17 09:30:35 -0500

    t
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -3449,7 +3449,8 @@ J9::Z::PrivateLinkage::addSpecialRegDepsForBuildArgs(TR::Node * callNode, TR::Re
 
    if (callNode->isJitDispatchJ9MethodCall(comp())) {
       specialArgReg = getJ9MethodArgumentRegister();
-      TR::Register *specialArg = cg()->evaluate(callNode->getChild(from)); // TODO:JSR292: We don't need a copy of the highOrder reg on 31-bit
+      child = callNode->getChild(from);
+      TR::Register *specialArg = cg()->evaluate(child); // TODO:JSR292: We don't need a copy of the highOrder reg on 31-bit
       if (specialArg->getRegisterPair())
          specialArg = specialArg->getLowOrder(); // on 31-bit, the top half doesn't matter, so discard it
       dependencies->addPreCondition(specialArg, specialArgReg);
