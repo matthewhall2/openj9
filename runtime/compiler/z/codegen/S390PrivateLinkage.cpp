384f1a56805b41a559d591131169ff5cb833365e
Matthew Hall <matthew.hall3@outlook.com>
2026-01-12 14:51:55 -0500

    test ool
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -2613,7 +2613,8 @@ J9::Z::PrivateLinkage::buildDirectCall(TR::Node * callNode, TR::SymbolReference 
       // always go through j2iTransition if stressJitDispatchJ9MethodJ2I is set
       TR::InstOpCode::S390BranchCondition oolBranchOp = cg()->stressJitDispatchJ9MethodJ2I() ? TR::InstOpCode::COND_BRC : TR::InstOpCode::COND_MASK1;
       bool useDeps = feGetEnv("useDeps") != NULL;
-      gcPoint = generateS390BranchInstruction(cg(), TR::InstOpCode::BRC, oolBranchOp, callNode, snippetLabel, useDeps ? dependencies : NULL);
+      bool toOOL = feGetEnv("toOOL") != NULL;
+      gcPoint = generateS390BranchInstruction(cg(), TR::InstOpCode::BRC, oolBranchOp, callNode, toOOL ? interpreterCallLabel : snippetLabel, useDeps ? dependencies : NULL);
       gcPoint->setNeedsGCMap(getPreservedRegisterMapForGC());
 
       // find target address
