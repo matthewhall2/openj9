4e7664fcdb46fd34cebff4155d726d0b631498ff
Matthew Hall <matthew.hall3@outlook.com>
2026-01-14 14:12:01 -0500

    t

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -3468,7 +3468,7 @@ J9::Z::PrivateLinkage::addSpecialRegDepsForBuildArgs(TR::Node * callNode, TR::Re
       if (specialArg->getRegisterPair())
          specialArg = specialArg->getLowOrder(); // on 31-bit, the top half doesn't matter, so discard it
       dependencies->addPreCondition(specialArg, specialArgReg);
-      dependencies->addPostCondition(specialArg, TR::RealRegister::AssignAny);
+      dependencies->addPostCondition(specialArg, specialArgReg);
       TR::Register *indexReg = cg()->allocateRegister();
    //   dependencies->addPreCondition(indexReg, getVTableIndexArgumentRegister());
       dependencies->addPostCondition(indexReg, getVTableIndexArgumentRegister());
