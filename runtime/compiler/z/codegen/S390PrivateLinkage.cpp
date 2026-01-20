b5153271593b5ec43fd1b4355c41850659446b72
Matthew Hall <matthew.hall3@outlook.com>
2026-01-20 15:42:16 -0500

    r8

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -115,7 +115,7 @@ J9::Z::PrivateLinkage::PrivateLinkage(TR::CodeGenerator * codeGen,TR_LinkageConv
    setReturnAddressRegister (TR::RealRegister::GPR14);
 
    setVTableIndexArgumentRegister (TR::RealRegister::GPR0);
-   setJ9MethodArgumentRegister    (TR::RealRegister::GPR1);
+   setJ9MethodArgumentRegister    (TR::RealRegister::GPR8);
 
    setLitPoolRegister       (TR::RealRegister::GPR6  );
    setMethodMetaDataRegister(TR::RealRegister::GPR13 );
@@ -3475,7 +3475,7 @@ J9::Z::PrivateLinkage::addSpecialRegDepsForBuildArgs(TR::Node * callNode, TR::Re
   //    TR::Register *indexReg = cg()->allocateRegister();
      // dependencies->addPreCondition(indexReg, getVTableIndexArgumentRegister());
   //    dependencies->addPostCondition(indexReg, getVTableIndexArgumentRegister());
-      cg()->decReferenceCount(child);
+    //  cg()->decReferenceCount(child);
       from += step;
       return;
    }
