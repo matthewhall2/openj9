6aa58237cb08ffdee7b277a23ecde405f96cddbb
Matthew Hall <matthew.hall3@outlook.com>
2026-01-23 16:35:15 -0500

    add post dep for j9m reg
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -3472,7 +3472,7 @@ J9::Z::PrivateLinkage::addSpecialRegDepsForBuildArgs(TR::Node * callNode, TR::Re
       if (specialArg->getRegisterPair())
          specialArg = specialArg->getLowOrder(); // on 31-bit, the top half doesn't matter, so discard it
       dependencies->addPreCondition(specialArg, specialArgReg);
-   //   dependencies->addPostCondition(specialArg, specialArgReg);
+      dependencies->addPostCondition(specialArg, specialArgReg);
   //    TR::Register *indexReg = cg()->allocateRegister();
      // dependencies->addPreCondition(indexReg, getVTableIndexArgumentRegister());
   //    dependencies->addPostCondition(indexReg, getVTableIndexArgumentRegister());
