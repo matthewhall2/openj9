fb3dadee106c9bd8bea8ac9bded8e6b87347f8ef
Matthew Hall <matthew.hall3@outlook.com>
2026-01-23 11:08:47 -0500

    test

--- runtime/compiler/x/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/x/codegen/J9TreeEvaluator.cpp
@@ -4086,6 +4086,7 @@ inline void generateInlineSuperclassTest(TR::Node* node, TR::CodeGenerator *cg, 
    TR::Instruction *cursor = NULL;
    if (toClassDepth == -1)
       {
+      cg->generateDebugCounter(TR::DebugCounter::debugCounterName(comp, "isAssignableFromStats/toClassDepthUnknown"), 1, TR::DebugCounter::Punitive);
       toClassDepthReg = srm->findOrCreateScratchRegister();
       cursor = generateRegMemInstruction(cg->comp()->target().is64Bit()? TR::InstOpCode::MOVZXReg8Mem2 : TR::InstOpCode::MOVZXReg4Mem2, node,
                toClassDepthReg, generateX86MemoryReference(toClassReg, offsetof(J9Class, classDepthAndFlags), cg), cg);
@@ -4099,9 +4100,11 @@ inline void generateInlineSuperclassTest(TR::Node* node, TR::CodeGenerator *cg, 
       }
    else
       {
+      cg->generateDebugCounter(TR::DebugCounter::debugCounterName(comp, "isAssignableFromStats/toClassDepthKnown"), 1, TR::DebugCounter::Punitive);
       // cast class depth >= obj class depth, return false
       generateMemImmInstruction(TR::InstOpCode::CMP2MemImm2, node,
             generateX86MemoryReference(fromClassReg, offsetof(J9Class, classDepthAndFlags), cg), toClassDepth, cg);
+      static auto jumpInstr = feGetEnv("jle") != NULL ? TR::InstOpCode::JLE4 : TR::InstOpCode::JBE4;
       cursor = generateLabelInstruction(TR::InstOpCode::JBE4, node, failLabel, cg);
       }
 
