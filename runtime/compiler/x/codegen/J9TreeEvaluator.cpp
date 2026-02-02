2593c36de361986ed687ae4b63023e89373d909f
Matthew Hall <matthew.hall3@outlook.com>
2026-02-02 17:25:33 -0500

    add ool
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/x/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/x/codegen/J9TreeEvaluator.cpp
@@ -4269,12 +4269,12 @@ inline TR::Register* generateInlinedIsAssignableFrom(TR::Node* node, TR::CodeGen
  //  only needed for array case
 
 
-      // TR_OutlinedInstructionsGenerator og(outlinedCallLabel, node, cg);
-      // cg->generateDebugCounter(TR::DebugCounter::debugCounterName(comp, "isAssignableFromStats/ArrayClassTest"), 1, TR::DebugCounter::Punitive);
-      // tempReg = TR::TreeEvaluator::performCall(node, false, false, cg);
-      // generateRegRegInstruction(TR::InstOpCode::MOVRegReg(), node, resultReg, tempReg, cg);
-      // generateLabelInstruction(TR::InstOpCode::JMP4, node, doneLabel, cg);
-      // og.endOutlinedInstructionSequence();
+      TR_OutlinedInstructionsGenerator og(outlinedCallLabel, node, cg);
+      cg->generateDebugCounter(TR::DebugCounter::debugCounterName(comp, "isAssignableFromStats/ArrayClassTest"), 1, TR::DebugCounter::Punitive);
+      tempReg = TR::TreeEvaluator::performHelperCall(node, NULL, TR::icall, false, cg);
+      generateRegRegInstruction(TR::InstOpCode::MOVRegReg(), node, resultReg, tempReg, cg);
+      generateLabelInstruction(TR::InstOpCode::JMP4, node, doneLabel, cg);
+      og.endOutlinedInstructionSequence();
      
 
    startLabel->setStartInternalControlFlow();
@@ -4305,9 +4305,9 @@ inline TR::Register* generateInlinedIsAssignableFrom(TR::Node* node, TR::CodeGen
       static bool onlyHelper = feGetEnv("onlyHelper") != NULL;
       if (onlyHelper) {
 
-          tempReg = TR::TreeEvaluator::performCall(node, false, false, cg);
-      generateRegRegInstruction(TR::InstOpCode::MOVRegReg(), node, resultReg, tempReg, cg);
-      generateLabelInstruction(TR::InstOpCode::JMP4, node, doneLabel, cg);
+       //   tempReg = TR::TreeEvaluator::performCall(node, false, false, cg);
+    //  generateRegRegInstruction(TR::InstOpCode::MOVRegReg(), node, resultReg, tempReg, cg);
+      generateLabelInstruction(TR::InstOpCode::JMP4, node, outlinedCallLabel, cg);
       }
 
       // no need for null checks. They are inserted prior to isAssignableFrom call in RecognizedCallTransformer
