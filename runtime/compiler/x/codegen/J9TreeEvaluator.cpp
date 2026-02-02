01124cb0972e471dc8c89d5e243d9ddc65c9ed93
Matthew Hall <matthew.hall3@outlook.com>
2026-02-02 12:50:44 -0500

    take out dec counts
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/x/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/x/codegen/J9TreeEvaluator.cpp
@@ -4392,8 +4392,8 @@ inline TR::Register* generateInlinedIsAssignableFrom(TR::Node* node, TR::CodeGen
    doneLabel->setEndInternalControlFlow();
    generateLabelInstruction(TR::InstOpCode::label, node, doneLabel, deps, cg);
    node->setRegister(resultReg);
-   // cg->decReferenceCount(node->getChild(0));
-   //    cg->recursivelyDecReferenceCount(node->getChild(1));
+   cg->recursivelyDecReferenceCount(node->getChild(0));
+      cg->recursivelyDecReferenceCount(node->getChild(1));
    return resultReg;
    }
 
