26e94b5cb84d1c7f23de825e7e49a528efc1f63d
Matthew Hall <matthew.hall3@outlook.com>
2026-02-03 09:43:20 -0500

    icall
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/optimizer/J9RecognizedCallTransformer.cpp
+++ runtime/compiler/optimizer/J9RecognizedCallTransformer.cpp
@@ -83,7 +83,7 @@ void J9::RecognizedCallTransformer::process_java_lang_Class_IsAssignableFrom(TR:
    node->setNumChildren(2);
 
    TR::SymbolReference *jitCheckAssignableSR = comp()->getSymRefTab()->findOrCreateRuntimeHelper(TR_checkAssignable);
-   TR::Node::recreateWithSymRef(node, node->getOpCode().getOpCodeValue(), jitCheckAssignableSR);
+   TR::Node::recreateWithSymRef(node, TR::icall, jitCheckAssignableSR);
 
    TR::SymbolReference *classFromJavaLangClassSR = comp()->getSymRefTab()->findOrCreateClassFromJavaLangClassSymbolRef();
 
