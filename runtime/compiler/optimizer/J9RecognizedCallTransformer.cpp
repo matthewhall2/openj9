b93e38464c60fc12bcfdb9012756bcb45e7551aa
Matthew Hall <matthew.hall3@outlook.com>
2026-02-03 09:49:28 -0500

    option to use original call op
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/optimizer/J9RecognizedCallTransformer.cpp
+++ runtime/compiler/optimizer/J9RecognizedCallTransformer.cpp
@@ -83,7 +83,8 @@ void J9::RecognizedCallTransformer::process_java_lang_Class_IsAssignableFrom(TR:
    node->setNumChildren(2);
 
    TR::SymbolReference *jitCheckAssignableSR = comp()->getSymRefTab()->findOrCreateRuntimeHelper(TR_checkAssignable);
-   TR::Node::recreateWithSymRef(node, TR::icall, jitCheckAssignableSR);
+   static bool useCallOp = feGetEnv("useCallOp") != NULL;
+   TR::Node::recreateWithSymRef(node, useCallOp ? node->getOpCode().getOpCodeValue() : TR::icall, jitCheckAssignableSR);
 
    TR::SymbolReference *classFromJavaLangClassSR = comp()->getSymRefTab()->findOrCreateClassFromJavaLangClassSymbolRef();
 
