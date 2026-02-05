3e2da609bd80620dcdb92c80272a7d3624fcfe8d
Matthew Hall <matthew.hall3@outlook.com>
2026-02-05 13:14:08 -0500

    helper call
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/x/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/x/codegen/J9TreeEvaluator.cpp
@@ -5287,7 +5287,7 @@ TR::Register *J9::X86::TreeEvaluator::checkcastinstanceofEvaluator(TR::Node *nod
 
          break;
       case TR::icall: // TR_checkAssignable
-         TR::SymbolReference * ref = callNode->getSymbolReference();
+         TR::SymbolReference * ref = node->getSymbolReference();
          TR_ASSERT_FATAL(ref == cg()->comp()->getSymRefTab()->findOrCreateRuntimeHelper(TR_checkAssignable), "must be checkAssignable\n");
          // disabled if TR_disableInliningOfIsAssignableFrom is set
          if (!useOld)
