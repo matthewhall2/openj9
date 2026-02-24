897a493177833ba51d878e331b84779d753ecc63
Matthew Hall <matthew.hall3@outlook.com>
2026-02-23 23:57:02 -0500

    t
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/x/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/x/codegen/J9TreeEvaluator.cpp
@@ -4261,10 +4261,8 @@ inline TR::Register *generateInlinedIsAssignableFrom(TR::Node *node, TR::CodeGen
    deps->stopAddingConditions();
    generateLabelInstruction(TR::InstOpCode::label, node, endLabel, deps, cg);
    node->setRegister(resultReg);
-   if (!fastFail && !fastPass) {
    cg->decReferenceCount(toClass);
    cg->decReferenceCount(fromClass);
-   }
    return resultReg;
    }
 
