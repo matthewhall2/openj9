527590506d2ec995ab63e3997b70582b0d325f35
Matthew Hall <matthew.hall3@outlook.com>
2026-03-30 16:53:47 -0400

    test r31
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
+++ runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
@@ -1436,10 +1436,11 @@ int32_t J9::Power::PrivateLinkage::buildPrivateLinkageArgs(TR::Node *callNode,
 
     if (isJitDispatchJ9Method) {
         specialArgReg = getProperties().getJ9MethodArgumentRegister();
-        TR::Register *cnd7Reg = cg()->allocateRegister(TR_CCR);
-        //TR::addDependency(dependencies, cnd7Reg, TR::RealRegister::cr7, TR_CCR, cg());
-        TR::addDependency(dependencies, NULL, TR::RealRegister::gr31, TR_GPR, cg());
-
+       // TR::Register *cnd7Reg = cg()->allocateRegister(TR_CCR);
+        TR::Register *scratch = cg()->allocateRegister(TR_GPR);
+     //   TR::addDependency(dependencies, cnd7Reg, TR::RealRegister::cr7, TR_CCR, cg());
+     dependencies->addPostCondition(scratch, TR::RealRegister::gr31);
+       // TR::addDependency(dependencies, NULL, TR::RealRegister::gr31, TR_GPR, cg());
     }
 
     if (specialArgReg != TR::RealRegister::NoReg) {
@@ -2767,7 +2768,7 @@ void J9::Power::PrivateLinkage::buildDirectCall(TR::Node *callNode, TR::SymbolRe
         auto regMapMask = pp.getPreservedRegisterMapForGC();
 
         TR::Register *scratchReg = dependencies->searchPostConditionRegister(pp.getVTableIndexArgumentRegister());
-        TR::Register *scratchReg2 = dependencies->searchPreConditionRegister(TR::RealRegister::gr31);
+        TR::Register *scratchReg2 = dependencies->searchPostConditionRegister(TR::RealRegister::gr31);
         TR::Register *cndReg = dependencies->searchPreConditionRegister(TR::RealRegister::cr0);
         //TR::Register *cnd7Reg = dependencies->searchPreConditionRegister(TR::RealRegister::cr7);
         TR::Register *j9MethodReg = dependencies->searchPreConditionRegister(pp.getJ9MethodArgumentRegister());
