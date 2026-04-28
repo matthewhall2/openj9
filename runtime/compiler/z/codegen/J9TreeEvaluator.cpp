4224f4b18f6c3d83d5a32d1c0ce8f1db3f710711
Matthew Hall <matthew.hall3@outlook.com>
2026-04-28 10:11:01 -0400

    optional itable
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/z/codegen/J9TreeEvaluator.cpp
@@ -4460,7 +4460,7 @@ TR::Register *J9::Z::TreeEvaluator::checkcastEvaluator(TR::Node *node, TR::CodeG
                 TR_ASSERT(flags < UINT_MAX && flags > 0,
                     "superclass test::(J9AccInterface | J9AccClassArray) is not a 32-bit number\n");
             
-                if (castClassDepth == -1) {
+                if (castClassDepth == -1 && cg->supportsInlineItableWalk()) {
                     TR::Register *modReg =  srm->findOrCreateScratchRegister();
                 generateRXInstruction(cg, TR::InstOpCode::getLoadOpCode(), node, modReg,
                     generateS390MemoryReference(castClassReg, offsetof(J9Class, romClass), cg));
@@ -4470,11 +4470,11 @@ TR::Register *J9::Z::TreeEvaluator::checkcastEvaluator(TR::Node *node, TR::CodeG
                 generateS390BranchInstruction(cg, TR::InstOpCode::BRC, TR::InstOpCode::COND_MASK7, node, callLabel);
                 generateRIInstruction(cg, TR::InstOpCode::TMLL, node, modReg, J9AccInterface);
                 generateS390BranchInstruction(cg, TR::InstOpCode::BRC, TR::InstOpCode::COND_MASK7, node, interfaceLabel);
-
-
                 // genTestModifierFlags(cg, node, castClassReg, castClassDepth, callLabel, srm, J9AccClassArray, modReg);
                 // genTestModifierFlags(cg, node, castClassReg, castClassDepth, interfaceLabel, srm, J9AccInterface, modReg);
                 srm->reclaimScratchRegister(modReg);
+                } else if (classDepth == -1) {
+                    genTestModifierFlags(cg, node, castClassReg, castClassDepth, callLabel, srm, flags);
                 }
 
                 genSuperclassTest(cg, node, castClassReg, castClassDepth, objClassReg, callLabel, srm);
@@ -8912,7 +8912,7 @@ TR::Register *J9::Z::TreeEvaluator::VMgenCoreInstanceofEvaluator(TR::Node *node,
                 TR::LabelSymbol *callHelperLabel
                     = (*(iter + 1) == DynamicCacheDynamicCastClassTest) ? dynamicCacheTestLabel : callLabel;
 
-                if (castClassDepth == -1) {
+                if (castClassDepth == -1 && cg->supportsInlineItableWalk()) {
                    // genTestModifierFlags(cg, node, castClassReg, castClassDepth, callLabel, srm, flags);
                     TR::Register *modReg =  srm->findOrCreateScratchRegister();
                 generateRXInstruction(cg, TR::InstOpCode::getLoadOpCode(), node, modReg,
@@ -8927,6 +8927,8 @@ TR::Register *J9::Z::TreeEvaluator::VMgenCoreInstanceofEvaluator(TR::Node *node,
                // genTestModifierFlags(cg, node, castClassReg, castClassDepth, callLabel, srm, J9AccClassArray, modReg);
                // genTestModifierFlags(cg, node, castClassReg, castClassDepth, interfaceLabel, srm, J9AccInterface, modReg);
                 srm->reclaimScratchRegister(modReg);
+                } else if (castClassDepth == -1) {
+                    genTestModifierFlags(cg, node, castClassReg, castClassDepth, callLabel, srm, flags);
                 }
 
                 genSuperclassTest(cg, node, castClassReg, castClassDepth, objClassReg, falseLabel, srm);
