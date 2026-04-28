517d3de4693ac5043746e0c5f1414b184595ad49
Matthew Hall <matthew.hall3@outlook.com>
2026-04-27 21:28:01 -0400

    est tmlh
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/z/codegen/J9TreeEvaluator.cpp
@@ -4466,8 +4466,14 @@ TR::Register *J9::Z::TreeEvaluator::checkcastEvaluator(TR::Node *node, TR::CodeG
                     generateS390MemoryReference(castClassReg, offsetof(J9Class, romClass), cg));
                 generateRXInstruction(cg, TR::InstOpCode::L, node, modReg,
                     generateS390MemoryReference(modReg, offsetof(J9ROMClass, modifiers), cg));
-                genTestModifierFlags(cg, node, castClassReg, castClassDepth, callLabel, srm, J9AccClassArray, modReg);
-                genTestModifierFlags(cg, node, castClassReg, castClassDepth, interfaceLabel, srm, J9AccInterface, modReg);
+                generateRIInstruction(cg, TR::InstOpCode::TMLH, node, modReg, J9AccClassArray);
+                generateS390BranchInstruction(cg, TR::InstOpCode::BRC, TR::InstOpCode::COND_BNE, node, callLabel);
+                generateRIInstruction(cg, TR::InstOpCode::TMLL, node, modReg, J9AccInterface);
+                generateS390BranchInstruction(cg, TR::InstOpCode::BRC, TR::InstOpCode::COND_BNE, node, interfaceLabel);
+
+
+                // genTestModifierFlags(cg, node, castClassReg, castClassDepth, callLabel, srm, J9AccClassArray, modReg);
+                // genTestModifierFlags(cg, node, castClassReg, castClassDepth, interfaceLabel, srm, J9AccInterface, modReg);
                 srm->reclaimScratchRegister(modReg);
                 }
 
@@ -8913,8 +8919,13 @@ TR::Register *J9::Z::TreeEvaluator::VMgenCoreInstanceofEvaluator(TR::Node *node,
                 generateS390MemoryReference(castClassReg, offsetof(J9Class, romClass), cg));
                 generateRXInstruction(cg, TR::InstOpCode::L, node, modReg,
                 generateS390MemoryReference(modReg, offsetof(J9ROMClass, modifiers), cg));
-                genTestModifierFlags(cg, node, castClassReg, castClassDepth, callLabel, srm, J9AccClassArray, modReg);
-                genTestModifierFlags(cg, node, castClassReg, castClassDepth, interfaceLabel, srm, J9AccInterface, modReg);
+                generateRIInstruction(cg, TR::InstOpCode::TMLH, node, modReg, J9AccClassArray);
+                generateS390BranchInstruction(cg, TR::InstOpCode::BRC, TR::InstOpCode::COND_BNE, node, callLabel);
+                generateRIInstruction(cg, TR::InstOpCode::TMLL, node, modReg, J9AccInterface);
+                generateS390BranchInstruction(cg, TR::InstOpCode::BRC, TR::InstOpCode::COND_BNE, node, interfaceLabel);
+
+               // genTestModifierFlags(cg, node, castClassReg, castClassDepth, callLabel, srm, J9AccClassArray, modReg);
+               // genTestModifierFlags(cg, node, castClassReg, castClassDepth, interfaceLabel, srm, J9AccInterface, modReg);
                 srm->reclaimScratchRegister(modReg);
                 }
 
@@ -11713,8 +11724,13 @@ TR::Register *J9::Z::TreeEvaluator::inlineCheckAssignableFromEvaluator(TR::Node 
             generateS390MemoryReference(toClassReg, offsetof(J9Class, romClass), cg));
             generateRXInstruction(cg, TR::InstOpCode::L, node, modReg,
             generateS390MemoryReference(modReg, offsetof(J9ROMClass, modifiers), cg));
-            genTestModifierFlags(cg, node, toClassReg, toClassDepth, helperCallLabel, srm, J9AccClassArray, modReg);
-            genTestModifierFlags(cg, node, toClassReg, toClassDepth, interfaceLabel, srm, J9AccInterface, modReg);
+generateRIInstruction(cg, TR::InstOpCode::TMLH, node, modReg, J9AccClassArray);
+                generateS390BranchInstruction(cg, TR::InstOpCode::BRC, TR::InstOpCode::COND_BNE, node, callLabel);
+                generateRIInstruction(cg, TR::InstOpCode::TMLL, node, modReg, J9AccInterface);
+                generateS390BranchInstruction(cg, TR::InstOpCode::BRC, TR::InstOpCode::COND_BNE, node, interfaceLabel);
+
+            //genTestModifierFlags(cg, node, toClassReg, toClassDepth, helperCallLabel, srm, J9AccClassArray, modReg);
+            //genTestModifierFlags(cg, node, toClassReg, toClassDepth, interfaceLabel, srm, J9AccInterface, modReg);
             srm->reclaimScratchRegister(modReg);
                 } else {
                     genTestModifierFlags(cg, node, toClassReg, toClassDepth, helperCallLabel, srm, flags);
