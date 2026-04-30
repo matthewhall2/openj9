0244f095587ae0b1dacffceceb2b629c03246c7a
Matthew Hall <matthew.hall3@outlook.com>
2026-04-30 13:57:59 -0400

    only pre dep
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/z/codegen/S390PrivateLinkage.cpp
+++ runtime/compiler/z/codegen/S390PrivateLinkage.cpp
@@ -2436,7 +2436,7 @@ TR::Instruction *J9::Z::PrivateLinkage::buildDirectCall(TR::Node *callNode, TR::
         TR::RegisterDependencyConditions *postDeps
             = new (trHeapMemory()) TR::RegisterDependencyConditions(postDepsTemp, 1, 0, cg());
 
-        deps->addPreCondition(j9MethodReg, getJ9MethodArgumentRegister());
+        dependencies->addPreCondition(j9MethodReg, getJ9MethodArgumentRegister());
 
         
         
