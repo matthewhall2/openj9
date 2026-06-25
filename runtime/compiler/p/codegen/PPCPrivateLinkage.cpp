05b4c00aad462a87d97698d71bc6365e77d0126e
Matthew Hall <matthew.hall3@outlook.com>
2026-06-24 23:31:22 -0400

    gen shift right

--- runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
+++ runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
@@ -2825,7 +2825,8 @@ void J9::Power::PrivateLinkage::buildDirectCall(TR::Node *callNode, TR::SymbolRe
             TR::MemoryReference::createWithDisplacement(cg(), scratchReg, -4,
                 TR::Compiler->om.sizeofReferenceAddress()));
         // srwi 16 = rlwinm rA, rS, 16, 16, 31  (unsigned shift right by 16, zeroing upper bits)
-        generateTrg1Src1Imm2Instruction(cg(), TR::InstOpCode::rlwinm, callNode, j9MethodReg, j9MethodReg, 16, 0x0000FFFF);
+        generateShiftRightLogicalImmediate(cg(), callNode, j9MethodReg, j9MethodReg, 16);
+       // generateTrg1Src1Imm2Instruction(cg(), TR::InstOpCode::rlwinm, callNode, j9MethodReg, j9MethodReg, 16, 0x0000FFFF);
         generateTrg1Src2Instruction(cg(), TR::InstOpCode::add, callNode, scratchReg, j9MethodReg, scratchReg);
         generateSrc1Instruction(cg(), TR::InstOpCode::mtctr, callNode, scratchReg);
         gcPoint = generateInstruction(cg(), TR::InstOpCode::bctrl, callNode);
