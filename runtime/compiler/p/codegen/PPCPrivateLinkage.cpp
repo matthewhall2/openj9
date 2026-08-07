13e259e753f3265402bca49860f036d6fdd6e3fd
Matthew Hall <matthew.hall3@outlook.com>
2026-08-07 12:11:13 -0400

    use gr12 for mtctr
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
+++ runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
@@ -2811,8 +2811,8 @@ void J9::Power::PrivateLinkage::buildDirectCall(TR::Node *callNode, TR::SymbolRe
         generateTrg1MemInstruction(cg(), TR::InstOpCode::lwz, callNode, j9MethodReg,
             TR::MemoryReference::createWithDisplacement(cg(), scratchReg, -4, 4));
         generateShiftRightLogicalImmediate(cg(), callNode, j9MethodReg, j9MethodReg, 16);
-        generateTrg1Src2Instruction(cg(), TR::InstOpCode::add, callNode, scratchReg2, scratchReg, j9MethodReg);
-        generateSrc1Instruction(cg(), TR::InstOpCode::mtctr, callNode, scratchReg2);
+        generateTrg1Src2Instruction(cg(), TR::InstOpCode::add, callNode, scratchReg, scratchReg, j9MethodReg);
+        generateSrc1Instruction(cg(), TR::InstOpCode::mtctr, callNode, scratchReg);
         gcPoint = generateInstruction(cg(), TR::InstOpCode::bctrl, callNode);
         gcPoint->PPCNeedsGCMap(regMapMask);
 
