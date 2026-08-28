669d010cffa417a1a31fae9ed4a6372f328f0a85
Matthew Hall <matthew.hall3@outlook.com>
2026-08-28 17:03:04 -0400

    Fix InterpreterEmulator handling of putfield
    changes handling of putfield to pop 2 args as required
    
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/optimizer/InterpreterEmulator.cpp
+++ runtime/compiler/optimizer/InterpreterEmulator.cpp
@@ -631,8 +631,10 @@ bool InterpreterEmulator::maintainStack(TR_J9ByteCode bc)
         case J9BCgoto:
             genTarget(bcIndex() + next2BytesSigned());
             break;
-        case J9BCpop:
         case J9BCputfield:
+            popn(2);
+            break;
+        case J9BCpop:
         case J9BCputstatic:
             pop();
             break;
